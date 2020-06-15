994c500b687e81232936d3900ba85e1cee903aab
Heidi Tran <HeidiTran1410@gmail.com>
2020-06-15 17:21:59 -0400

    Set up PeerJS

--- rapid-chat-client/src/app/home/home.component.ts
+++ rapid-chat-client/src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const Peer: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  peer: any;
+  myid: any;
+  anotherid: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.peer = new Peer();
+
+    setTimeout(() => {
+      this.myid = this.peer.id;
+    }, 3000);
+
+    this.peer.on('connection', conn => {
+      conn.on('data', data => {
+        console.log(data);
+      })
+    });
+  }
+
+  connect() {
+    var conn = this.peer.connect(this.anotherid);
+    conn.on('open', () => {
+      console.log("Sending messages...");
+      conn.send('hi');
+    });
   }
 
 }
