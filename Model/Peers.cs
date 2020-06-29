2a6b076565f40ba027417d9498e9bdcc0e771032
Heidi Tran <HeidiTran1410@gmail.com>
2020-06-28 22:33:38 -0400

    Recreate DataContext and Model using scaffolding

---
+++ Model/Peers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RapidChat
+{
+    public partial class Peers
+    {
+        public long Id { get; set; }
+        public string PeerId { get; set; }
+        public string RoomName { get; set; }
+
+        public Rooms RoomNameNavigation { get; set; }
+    }
+}
---
+++ Model/Rooms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RapidChat
+{
+    public partial class Rooms
+    {
+        public Rooms()
+        {
+            Peers = new HashSet<Peers>();
+        }
+
+        public string RoomName { get; set; }
+
+        public ICollection<Peers> Peers { get; set; }
+    }
+}
