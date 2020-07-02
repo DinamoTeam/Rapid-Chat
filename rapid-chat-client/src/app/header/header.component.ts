7756c0fb11e06c9367efb0d7fee5fbe055893f93
Heidi Tran <HeidiTran1410@gmail.com>
2020-07-02 08:05:46 -0400

    Prettier

--- rapid-chat-client/src/app/header/header.component.ts
+++ rapid-chat-client/src/app/header/header.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { windowWhen } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { windowWhen } from "rxjs/operators";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
   isExpanded = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   collapse() {
     this.isExpanded = false;
@@ -25,27 +24,25 @@ export class HeaderComponent implements OnInit {
 
   showSuccessAlert = false;
   onBtnHomeClick() {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
+    const selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
     selBox.value = window.location.href;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(selBox);
 
     this.showSuccessAlert = true;
     this.isExpanded = false;
     alert("Link copied to clipboard!");
-	
   }
 
   goHome() {
     this.isExpanded = false;
     window.location.replace("/");
-
   }
 }
--- rapid-chat-client/src/app/services/room.service.ts
+++ rapid-chat-client/src/app/services/room.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { retry, catchError } from "rxjs/operators";
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
